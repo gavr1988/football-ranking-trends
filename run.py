3e54afba5d7ad5dcd019717e9e1d9b619f2a8bec
gavr1988 <gavrewt@gmail.com>
2026-08-24 09:59:51 +0100

    file structure and initial file paths

---
+++ run.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+# -----------------------------
+# File paths
+# -----------------------------
+RAW_PATH = Path("data/raw/fifa_ranking.csv")
+OUTPUT_PATH = Path("data/cleaned/fifa_ranking_clean.csv")
